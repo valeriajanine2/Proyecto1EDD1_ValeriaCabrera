6fb92340eadd0379954d3590399161d29b80a9d6
Valeria Cabrera <valeriajanine1@gmail.com>
2021-08-17 01:17:29 -0600

    Solo era de declarar todos los metodos :s

--- Proyecto1EDD1_ValeriaCabrera/ListaEnlazada.cpp
+++ Proyecto1EDD1_ValeriaCabrera/ListaEnlazada.cpp
@@ -1,2 +1,54 @@
 #include "ListaEnlazada.hpp"
 
+ListaEnlazada::ListaEnlazada():Lista(){
+	
+}
+
+ListaEnlazada::ListaEnlazada(Nodo* head, int length){
+	
+}
+
+ListaEnlazada::~ListaEnlazada(){
+	
+}
+
+int ListaEnlazada::FIN(Lista* L){
+	return 1;
+}
+
+void ListaEnlazada::INSERTA(Item* x, int p, Lista* L){
+	
+}
+
+int ListaEnlazada::LOCALIZA(Item* x, Lista* L){
+	return 1;
+}
+
+Item* ListaEnlazada::RECUPERA(int p, Lista* L){
+	Item* n;
+	return n;
+}
+
+void ListaEnlazada::SUPRIME(int p, Lista* L){
+	
+}
+
+int ListaEnlazada::SIGUIENTE(int p, Lista* L){
+	return 1;
+}
+
+int ListaEnlazada::ANTERIOR(int p, Lista* L){
+	return 1;
+}
+
+int ListaEnlazada::ANULA(Lista* L){
+	return L->FIN(L);
+}
+
+int ListaEnlazada::PRIMERO(Lista* L){
+	return 1;
+}
+
+void ListaEnlazada::IMPRIME_LISTA(Lista* L){
+	
+}
